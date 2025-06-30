17475a7f4333fd464ec5088631242f3a22848782
Perry Chamileke <pchamileke@gmail.com>
2025-07-01 01:38:58 +0200

    fixed alignment issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import Todolist from './components/todo-list';
 import './styles/styles.css'
+import {useState} from 'react';
 function App(){
+  const [theme, setTheme] = useState("dark");
+  function changeTheme(){
+   if(theme === "dark") setTheme("light"); else setTheme("dark")
+  }
   return(
   <div className="main-body">
       <header className="header">
         <h1 className="heading">TODO</h1>
-        <img src="/images/icon-moon.svg"/>
+        <img onClick={() => changeTheme()} src={theme == "dark" ? "/images/icon-moon.svg" : "/images/icon-sun.svg"}/>
       </header>
       <div className="image dark"></div>
     <Todolist/>
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -2,7 +2,7 @@ function Todo({value, done, onDelete, onComplete, id}) {
   return (
   <li className={`todo ${done ? "completed" : ""}`}>
    <div className="todo-p-wrapper">
-  <span className={`indicator ${done ? "complete": ""}`} onClick={() => onComplete(id)}></span>
+  <span className={`indicator dark-mode-bg ${done ? "complete": ""}`} onClick={() => onComplete(id)}></span>
     <p className="todo-text">{value}</p>
     </div>
     <button className="delete-todo" onClick={() => onDelete(id)}><img src="/images/icon-cross.svg"/></button>
